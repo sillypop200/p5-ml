bca9688fb71581276ba6df3659f415affccea660
Bonnie <bonhuynh@umich.edu>
2024-04-19 17:29:07 -0400

    fds

--- main.cpp
+++ main.cpp
@@ -5,7 +5,42 @@
 #include "csvstream.hpp" 
 using namespace std;
 
+class Classifier {
+private: 
+    bool debug; // 1 is debug 0 is don't debug 
+    int numPosts;
+    int uniqueWords; 
+    map <string,int> numPostsContainingWord; 
+    map <string,int> numPostsWithLabel;
+    map <pair<string,string>, int> numPostWithLabelThatContainsWord;
+public: 
+    Classifier (bool debug) 
+    : debug (debug), numPosts (0),uniqueWords(0){
+
+    } 
+    void print (string input ){
+        if (debug){
+            cout << input << endl; 
+        }
+    }
+    void train (csvstream& stream){
+        print ("training data:"); 
+        map<string,string> row;  
+        while (stream >> row){
+            ++numPosts;
+            print ("  label = " + row["tag"] + ", content = "+ row["content"]);
+            
+
+
+
+    
+        }
+        cout << "trained on " << to_string(numPosts) << " examples" << endl;
+        print ("vocabulary size = " + to_string(uniqueWords));
+    }
+};
 int main(int argc, char * argv []) {
+    cout.precision(3); 
     if (argc != 3 && argc != 4){
         cout << "Usage: main.exe TRAIN_FILE TEST_FILE [--debug]" << endl;
         return -1; // 69
